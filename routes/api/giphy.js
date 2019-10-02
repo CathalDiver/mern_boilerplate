4de1d8e2fb6704f04ef3a64df7109b416560ab55
cathal diver <cathaldiv93@gmail.com>
2019-10-01 17:07:59 -0700

    bug fixes
    bug fixes.

--- routes/api/giphy.js
+++ routes/api/giphy.js
@@ -10,7 +10,7 @@ router.get("/", auth, async (req, res) => {
 	try {
 		axios
 			.get(
-				"http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=LEls10pQ1lqL1lxLehiPoD0P61SpGcQK&limit=6"
+				"http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=[KEY_NEEDED]&limit=6"
 			)
 			.then(response => {
 				res.send(response.data);
@@ -26,7 +26,7 @@ router.get("/", auth, async (req, res) => {
 // @access   Public
 router.post("/search", async (req, res) => {
 	let str1 = "http://api.giphy.com/v1/gifs/search?q=";
-	const str2 = "&api_key=LEls10pQ1lqL1lxLehiPoD0P61SpGcQK&limit=6";
+	const str2 = "&api_key=[KEY_NEEDED]&limit=6";
 	str1 = str1.concat(req.body.searchTerm);
 	const url = str1.concat(str2).toString();
 
